b8f8b7521963e59c6d83caeb761cebdf54165093
BravishkaSkytano <65601594+BravishkaSkytano@users.noreply.github.com>
2026-03-11 15:25:23 -0400

    update tag pages

--- content.11tydata.js
+++ content.11tydata.js
@@ -1,6 +1,34 @@
 export default {
   layout: "page",
   eleventyComputed: {
+    // tags: (data) => {
+    //   // normalize tags to an array
+    //   const input = Array.isArray(data.tags)
+    //     ? data.tags
+    //     : [data.tags].filter(Boolean);
+
+    //   const expanded = new Set();
+
+    //   input.forEach((tag) => {
+    //     if (typeof tag !== "string") return;
+
+    //     // keep the original tag
+    //     expanded.add(tag);
+
+    //     // expand hierarchical tags
+    //     if (tag.includes("/")) {
+    //       const parts = tag.split("/");
+    //       let current = "";
+
+    //       parts.forEach((part) => {
+    //         current = current ? current + "/" + part : part;
+    //         expanded.add(current);
+    //       });
+    //     }
+    //   });
+
+    //   return [...expanded];
+    // },
     permalink: (data) => {
       // Respect frontmatter permalink
       if (data.permalink) {
